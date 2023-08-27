0e2e54bb05844429d820e42f2fa474b8ae7d8022
olaniyi victor <victorolaniyi22@gmail.com>
2023-08-27 23:01:43 +0100

    todoapp

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -2,7 +2,14 @@ import PropTypes from 'prop-types';
 import './index.css'
 import Item from "./Item";
 export default function PackingList({items,onDeleteItem,Darkmode,onToggleItems,onClearList}){
+  //writing it to show the list of the item
+  const all =items.length
+  //writing it to filter the remaining items when it is not checked
   const remainingItems = items.filter((item) => !item.checked).length;
+  //writing it to filter the items chosen when checked
+  const vic=  items.filter((item) => item.checked).length;
+  //writing it to filter the items remaining when it is not checked
+  const sam = items.filter((item) => !item.checked).length;
   return(
     <div >
       <ul >
@@ -16,9 +23,9 @@ export default function PackingList({items,onDeleteItem,Darkmode,onToggleItems,o
          <div className={`${Darkmode?`flex-man`:'flex-mans'}`}>
         <div>{`${remainingItems}`} items left</div>
         <div className='flexman'>
-        <div className='all'>All</div>
-        <div className={`${Darkmode ? `active`:`actives`}`}>Active</div>
-        <div>Completed</div>
+        <div className='all'> {`${all}`} All</div>
+        <div className={`${Darkmode ? `active`:`actives`}`}>{`${sam}`} Active</div>
+        <div>{`${vic}`} Completed</div>
         </div>
         <div onClick={onClearList} className={`${Darkmode ? `completed`:``}`}>Clear Completed</div>
        </div> 
